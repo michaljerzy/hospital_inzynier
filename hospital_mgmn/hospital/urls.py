bfc0ce60e455dbc4ed5d8d140444ed1ec6af48bb
michaljerzy <michal.jerzy99@gmail.com>
2021-10-16 19:59:33 +0200

    new version

--- hospital_mgmn/hospital/urls.py
+++ hospital_mgmn/hospital/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hospital.views import About, Home
+from hospital.views import About, Home, Contact
 
 urlpatterns = [
     path('',Home, name='home'),
-    path('about/', About, name='about')
+    path('about/', About, name='about'),
+    path('contact/', Contact, name='contact')
 ]
