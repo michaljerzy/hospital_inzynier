790a880ec9c50a737e9c2778ffcdb2817f484230
michaljerzy <michal.jerzy99@gmail.com>
2021-10-17 13:50:34 +0200

    add views

--- hospital_mgmn/hospital/views.py
+++ hospital_mgmn/hospital/views.py
@@ -1,11 +1,11 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# # Create your views here.
-# def About(request):
-#     return render(request, 'about.html')
-#
-# def Home(request):
-#     return render(request, 'home.html')
-#
-# def Contact(request):
-#     return render(request, 'contact.html')
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def About(request):
+    return render(request, 'about.html')
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
