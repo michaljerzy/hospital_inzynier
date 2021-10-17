82695df93504b275b2daa86055f8d420db6d1bd5
michaljerzy <michal.jerzy99@gmail.com>
2021-10-17 13:29:36 +0200

    don't use admin

--- hospital_mgmn/hospital_mgmn/settings.py
+++ hospital_mgmn/hospital_mgmn/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qdg&dkm$4!k51h)a5czkwn0(w9mj)(y^yr6j7ylcl950%8morh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["srv07.mikr.us"]
 
 
 # Application definition
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
