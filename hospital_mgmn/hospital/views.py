4fb34c9c6464d93153ddeb784b8ecd5706bdaa47
Michal Andrzejewski <Michal.Andrzejewski@covance.com>
2021-12-19 16:38:25 +0100

    Changes for Olcia

--- hospital_mgmn/hospital/views.py
+++ hospital_mgmn/hospital/views.py
@@ -565,12 +565,10 @@ def reject_appointment_view(request,pk):
 @login_required(login_url='doctorlogin')
 @user_passes_test(is_doctor)
 def doctor_dashboard_view(request):
-    #for three cards
     patientcount=models.Patient.objects.all().filter(status=True,assignedDoctorId=request.user.id).count()
     appointmentcount=models.Appointment.objects.all().filter(status=True,doctorId=request.user.id).count()
     patientdischarged=models.PatientDischargeDetails.objects.all().distinct().filter(assignedDoctorName=request.user.first_name).count()
 
-    #for  table in doctor dashboard
     appointments=models.Appointment.objects.all().filter(status=True,doctorId=request.user.id).order_by('-id')
     patientid=[]
     for a in appointments:
@@ -619,7 +617,7 @@ def doctor_view_discharge_patient_view(request):
 @login_required(login_url='doctorlogin')
 @user_passes_test(is_doctor)
 def doctor_appointment_view(request):
-    doctor=models.Doctor.objects.get(user_id=request.user.id) #for profile picture of doctor in sidebar
+    doctor=models.Doctor.objects.get(user_id=request.user.id)
     return render(request,'hospital/doctor_appointment.html',{'doctor':doctor})
 
 
@@ -641,7 +639,7 @@ def doctor_view_appointment_view(request):
 @login_required(login_url='doctorlogin')
 @user_passes_test(is_doctor)
 def doctor_delete_appointment_view(request):
-    doctor=models.Doctor.objects.get(user_id=request.user.id) #for profile picture of doctor in sidebar
+    doctor=models.Doctor.objects.get(user_id=request.user.id)
     appointments=models.Appointment.objects.all().filter(status=True,doctorId=request.user.id)
     patientid=[]
     for a in appointments:
@@ -860,7 +858,3 @@ def contactus_view(request):
             return render(request, 'hospital/contactussuccess.html')
     return render(request, 'hospital/contactus.html', {'form':sub})
 
-
-#---------------------------------------------------------------------------------
-#------------------------ ADMIN RELATED VIEWS END ------------------------------
-#---------------------------------------------------------------------------------
